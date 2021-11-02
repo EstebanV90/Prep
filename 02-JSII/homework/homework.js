5287c5ff49ae4c467bb96714d1ecf7d6523dde2f
EstebanV90 <92452431+EstebanV90@users.noreply.github.com>
2021-11-02 18:44:49 -0300

    Update homework.js

--- 02-JSII/homework/homework.js
+++ 02-JSII/homework/homework.js
@@ -3,21 +3,23 @@
 function devolverUltimoElemento(array) {
   // Devuelve el último elemento de un array
   // Tu código:
-
+  return array[array.length - 1]
 }
 
 function incrementarPorUno(array) {
   // "array" debe ser una arreglo de números enteros
   // Aumenta cada número por 1 y devuelve el array
   // Tu código:
-
+  for (var i = 0; i < array.length; i++) {
+    array[i] = array[i] + 1;
 }
 
 function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
-
+  array.push(elemento);
+  return array;
 }
 
 function dePalabrasAFrase(palabras) {
@@ -26,35 +28,54 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
-
+  var frase = palabras[0]
+  for (var i = 0; i < palabras.length; i++) {
+    frase = frase + " " + palabras[i]
+  }
+  return frase;
 }
 
 function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
-
+  return array.includes(elemento);
 }
 
 function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser un array de enteros
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
-
+  var j = 0;
+  for (var i = 0; i < array.length; i++) {
+    j = j + resultadosTest[i]
+  }
+  return j / resultadosTest.length;
 }
 
 function numeroMasGrande(numeros) {
   // "numeros" debe ser un array de enteros
   // Devuelve el número más grande
   // Tu código:
-
-}
+  var max = 0;
+  for (var i = 0; i < numeros.length; i++) {
+    if (numeros[i]> max) 
+    max = numeros[i]
+  }
+  return max;
+} 
 
 function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Tu código:
+  if(arguments.length === 0) return 0;
 
+  var acum = 1;
+  for (var i = 0; i < arguments.length; i++) {
+    acum = acum * arguments[i];
+  }
+  return acum;
 }
 
 function cuentoElementos(arreglo){
